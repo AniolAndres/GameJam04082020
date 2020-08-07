74609fd2a81b734fbfe5386b14f7ae5ed2a04fd6
Alfonso <alfonsomestres@gmail.com>
2020-08-07 16:38:55 +0200

    Basic GrayBox with Baisc movement and warning text

--- GameJam/Assets/Scripts/VictimBehavior.cs
+++ GameJam/Assets/Scripts/VictimBehavior.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class VictimBehavior : MonoBehaviour
 {
+    public float minCheckTimer = 2.0f;
+
+    public GameObject detectionWarning;
+
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        RandomDetection();  
+    }
+
+    void RandomDetection()
+    {
+        if (Random.Range(0, 10) > 5 && detectionWarning.activeSelf)
+        {
+            StartCoroutine(TurnAndLook(Random.Range(2, 6))); //This could be linked to difficulty
+        } 
+        else
+        {
+            Debug.Log("Nothing to see");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+     IEnumerator TurnAndLook(int seconds)
     {
-        
+        detectionWarning.SetActive(true);
+        Debug.Log("Yo' something happening " + seconds);
+        yield return new WaitForSeconds(seconds);
+        detectionWarning.SetActive(false);
     }
 }
