27518ac885872a5ee5dddb7d1a62b55e4bc757b3
Roger <rogernogueb@gmail.com>
2020-08-07 18:56:56 +0200

    First version of Roger scene where the enemy IA is being built

--- GameJam/Assets/Scripts/VictimBehavior.cs
+++ GameJam/Assets/Scripts/VictimBehavior.cs
@@ -1,37 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class VictimBehavior : MonoBehaviour
 {
-    public float minCheckTimer = 2.0f;
+    ///////////////// Types
+    public enum eAlertState : UInt16
+    {
+        Safe = 0,               //The victim feels completely safe and does not get scared easily.
+        LowAlert,               //The victim has a slight suspicion about what is going on.
+        MediumAlert,            //The victim does not seem to trust the robber.
+        HighAlert,              //The victim is highly suspicious about the robber.
+        Escaping,               //The victim is running from the robber.
+        NumberOfStates          //the amount of states
+    };
 
-    public GameObject detectionWarning;
+    ///////////////// Vars
+    public float VisionDistance;
+    public float VisionAngle;
+    public float mTimeInProximityToAlertUp;
+    public float mTimeInVisionToAlertUp;
+    private float mTimeInVision = 0.0f;
+    private float mTimeInProximity = 0.0f;
+
+    public eAlertState CurrentState = eAlertState.Safe;
+
+    public GameObject mDetectionWarning;     //visual alert of the current alert state
+    public GameObject mRobber;               //reference to the robber
+
+    ///////////////// Methods
 
     // Start is called before the first frame update
     void Update()
     {
-        RandomDetection();  
+        VisualDetection();
+        ProximityDetection();
     }
 
-    void RandomDetection()
+    void VisualDetection()
     {
-        if (Random.Range(0, 10) > 5 && detectionWarning.activeSelf)
-        {
-            StartCoroutine(TurnAndLook(Random.Range(2, 6))); //This could be linked to difficulty
-        } 
-        else
-        {
-            Debug.Log("Nothing to see");
-        }
+
     }
 
-    // Update is called once per frame
-     IEnumerator TurnAndLook(int seconds)
+    void ProximityDetection()
     {
-        detectionWarning.SetActive(true);
+
+    }
+
+    IEnumerator TurnAndLook(int seconds)
+    {
+        mDetectionWarning.SetActive(true);
         Debug.Log("Yo' something happening " + seconds);
         yield return new WaitForSeconds(seconds);
-        detectionWarning.SetActive(false);
+        mDetectionWarning.SetActive(false);
     }
 }
