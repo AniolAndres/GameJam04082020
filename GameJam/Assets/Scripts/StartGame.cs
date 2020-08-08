e0c2b9288c440f604e9cd7c6f3df8534ff1145d0
Alfonso <alfonsomestres@gmail.com>
2020-08-08 11:24:24 +0200

    Go text on GameStart, disabled movement before start and camera follow

--- GameJam/Assets/Scripts/StartGame.cs
+++ GameJam/Assets/Scripts/StartGame.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class StartGame : MonoBehaviour
 {
+    public int maxGoSize = 150;
     public float cameraRotationSpeed = 2.5f;
     public GameObject gameCamera;
     public GameObject victimPosition;
     public GameObject playerPosition;
 
+    // Camera behavior
+    public Vector3 cameraFollowDisplacement = new Vector3(0, 2.5f, -0);
+    public float smoothTimeCameraFollow = 0.3F;
+
+    private Text goText;
     private bool isPreviewingVictim;
     private bool isLerpingToPlayer;
+    private bool isEnablingPlayerMovement;
+    private bool cameraFollowPlayer;
+    private Vector3 velocity = Vector3.zero;
 
     void Start()
     {
+        goText = transform.GetComponentInChildren<Text>();
         StartCoroutine("EnableVictimPreview");
     }
 
@@ -27,6 +39,17 @@ public class StartGame : MonoBehaviour
         if (isLerpingToPlayer)
         {
             LerpCameraToPlayer();
+        }
+
+        if(isEnablingPlayerMovement)
+        {
+            StartTextFadingAndUI();
+        }
+
+        if(cameraFollowPlayer)
+        {
+            Vector3 targetPosition = playerPosition.transform.TransformPoint(cameraFollowDisplacement);
+            gameCamera.transform.position = Vector3.SmoothDamp(gameCamera.transform.position, targetPosition, ref velocity, smoothTimeCameraFollow);
         }
     }
 
@@ -48,14 +71,10 @@ public class StartGame : MonoBehaviour
         gameCamera.transform.position = position;
     }
 
-    IEnumerator EnableLerpCameraToPlayer()
+    void StartTextFadingAndUI()
     {
-        isLerpingToPlayer = true;
-
-        while (gameCamera.transform.position.z >= playerPosition.transform.position.z + 1)
-            yield return null;
-
-        isLerpingToPlayer = false;
+        if(goText.fontSize <= 150)
+            goText.fontSize += 2;
     }
 
     IEnumerator EnableVictimPreview()
@@ -71,5 +90,32 @@ public class StartGame : MonoBehaviour
 
         StartCoroutine("EnableLerpCameraToPlayer");
     }
+
+    IEnumerator EnableLerpCameraToPlayer()
+    {
+        isLerpingToPlayer = true;
+
+        while (gameCamera.transform.position.z >= playerPosition.transform.position.z + 1)
+            yield return null;
+
+        isLerpingToPlayer = false;
+
+        StartCoroutine("EnablePlayerMovement");
+        
+    }
+
+    IEnumerator EnablePlayerMovement()
+    {
+        isEnablingPlayerMovement = true;
+
+        while (goText.fontSize < maxGoSize)
+            yield return null;
+
+        cameraFollowPlayer = true;
+        isEnablingPlayerMovement = false;
+        playerPosition.transform.parent.GetComponent<PlayerBehavior>().enabled = true;
+    }
+
+
 
 }
